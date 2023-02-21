cb9b191c377b5ac1608eae5e8cfb993ca7b64913
dsd350 <cfldec@163.com>
2023-02-21 16:42:09 +0800

    style(framework-主项目): 💄 文件上传代码优化

--- src/components/BigfileUpload/index.vue
+++ src/components/BigfileUpload/index.vue
@@ -1,226 +1,14 @@
 <script lang="tsx">
-import { ref, defineComponent } from 'vue'
-import { fetchStandard } from '@/http/standard'
-import SparkMD5 from 'spark-md5'
-import { useMessage } from 'naive-ui'
-import type { MessageReactive } from 'naive-ui'
+import { defineComponent } from 'vue'
+import { props } from './props'
+import { useHandleFileChange } from '@/hook'
 
 export default defineComponent({
   name: 'BigfileUpload',
-  props: {
-    reconnecNumber: {
-      type: Number,
-      default: 3,
-    },
-    maxSize: {
-      type: Number,
-      default: 1,
-    },
-    maxCount: {
-      type: Number,
-      default: 100,
-    },
-  },
+  props,
   setup(props) {
-    const message = useMessage()
-    const shortMessage = useMessage()
-    const disable = ref(false)
-    const processValue = ref(0)
-    const fileDom = ref<HTMLInputElement | null>(null)
-    let messageReactive: MessageReactive | null = null
-
-    type ChunkFileList = {
-      file: Blob
-      index: number
-    }[]
-
-    const stateReset = () => {
-      disable.value = false
-      processValue.value = 0
-    }
-
-    const calculateHashByFilechunks = (chunks: ChunkFileList) => {
-      return new Promise(resolve => {
-        const spark = new SparkMD5.ArrayBuffer()
-        let count = 0
-
-        const appendToSpark = (file: Blob) => {
-          return new Promise(resolve => {
-            const reader = new FileReader()
-            reader.readAsArrayBuffer(file)
-            reader.onload = ev => {
-              const buffer = ev.target.result
-              spark.append(buffer as ArrayBuffer)
-              resolve({})
-            }
-          })
-        }
-
-        const workLoop = async (deadline: IdleDeadline) => {
-          if (count < chunks.length && deadline.timeRemaining() > 0) {
-            await appendToSpark(chunks[count].file)
-            count++
-          }
-
-          if (count === chunks.length) {
-            resolve(spark.end())
-          }
-
-          window.requestIdleCallback(workLoop)
-        }
-
-        window.requestIdleCallback(workLoop)
-      })
-    }
-
-    const createFileChunk = (file: File) => {
-      let max = 1024 * 1024 * props.maxSize
-      let count = Math.ceil(file.size / max)
-      let index = 0
-      const chunks: ChunkFileList = []
-
-      if (count > props.maxCount) {
-        max = file.size / props.maxCount
-        count = props.maxCount
-      }
-
-      while (index < count) {
-        chunks.push({
-          file: file.slice(index * max, (index + 1) * max),
-          index,
-        })
-        index++
-      }
-
-      return chunks
-    }
-
-    const fileChange = async () => {
-      const file = fileDom.value.files[0]
-      if (!file) return
-
-      const suffix = /\.([a-zA-Z0-9]+)$/.exec(file.name)[1]
-      messageReactive = message.loading('资源解析中', {
-        duration: 0,
-      })
-      disable.value = true
-
-      const chunkFileList = createFileChunk(file)
-      const hash = await calculateHashByFilechunks(chunkFileList)
-
-      if (messageReactive) {
-        messageReactive.destroy()
-        messageReactive = null
-      }
-
-      const newChunkFileList = chunkFileList.map(item => {
-        return {
-          file: item.file,
-          key: item.index,
-          filename: `${hash}_${item.index + 1}.${suffix}`,
-        }
-      })
-
-      let alReady: Array<string> = []
-
-      try {
-        const data: any = await fetchStandard({
-          method: 'get',
-          url: 'http://127.0.0.1:8888/upload_already',
-          params: { HASH: hash },
-        })
-        if (+data.code === 200) {
-          alReady = data.fileList
-        }
-      } catch (error) {}
-
-      const complate = async (index: number) => {
-        index++
-        processValue.value = index
-
-        if (index < newChunkFileList.length) return
-        processValue.value = 100
-
-        try {
-          const data: any = await fetchStandard({
-            method: 'post',
-            url: 'http://127.0.0.1:8888/upload_merge',
-            data: { HASH: hash, count: newChunkFileList.length },
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-          })
-          if (+data.code === 200) {
-            shortMessage.info(
-              `恭喜您，文件上传成功，您可以基于 ${data.servicePath} 访问该文件~~`
-            )
-            stateReset()
-            return
-          }
-        } catch (error) {
-          shortMessage.error(`切片合并失败，请您稍后再试~~`)
-          stateReset()
-        }
-      }
-
-      const pool: Array<Promise<any>> = []
-      let isBreak = false
-      for (let i = 0; i < newChunkFileList.length; i++) {
-        if (isBreak) break
-        if (
-          alReady.length > 0 &&
-          alReady.includes(newChunkFileList[i].filename)
-        ) {
-          complate(i)
-          continue
-        }
-
-        const fm = new FormData()
-        fm.append('file', newChunkFileList[i].file)
-        fm.append('filename', newChunkFileList[i].filename)
-
-        const task = fetchStandard({
-          method: 'post',
-          url: 'http://127.0.0.1:8888/upload_chunk',
-          data: fm,
-        })
-
-        task
-          .then((data: any) => {
-            if (+data.code === 200) {
-              const indexT = pool.findIndex(t => t === task)
-              pool.splice(indexT)
-              complate(i)
-              return
-            }
-            return Promise.reject(data.codeText)
-          })
-          .catch(_error => {
-            isBreak = true
-            shortMessage.error(`当前切片上传失败，请稍后再试~~2`)
-            stateReset()
-          })
-
-        pool.push(task)
-
-        if (pool.length >= 5) {
-          try {
-            await Promise.race(pool)
-          } catch (error) {
-            shortMessage.error(`当前切片上传失败，请稍后再试~~1`)
-            stateReset()
-            break
-          }
-        }
-      }
-      fileDom.value.value = ''
-    }
-
-    // 选择文件
-    const selectFile = () => {
-      if (disable.value) return
-      fileDom.value.click()
-    }
+    const { fileRef, loading, progress, analogClick, fileChange } =
+      useHandleFileChange(props)
 
     return () => (
       <div class="container">
@@ -229,7 +17,7 @@ export default defineComponent({
           <section class="upload_box" id="upload7">
             <input
               type="file"
-              ref={fileDom}
+              ref={fileRef}
               class="upload_inp"
               onChange={fileChange}
             />
@@ -238,18 +26,18 @@ export default defineComponent({
                 class={[
                   'upload_button',
                   'select',
-                  disable.value ? 'disable' : '',
+                  loading.value ? 'disable' : '',
                 ]}
-                onClick={selectFile}
+                onClick={analogClick}
               >
                 上传图片
               </button>
             </div>
-            {processValue.value > 0 && (
+            {progress.value > 0 && (
               <div class="upload_progress">
                 <div
                   class="value"
-                  style={{ width: `${processValue.value}%` }}
+                  style={{ width: `${progress.value}%` }}
                 ></div>
               </div>
             )}
@@ -294,23 +82,6 @@ export default defineComponent({
     cursor: pointer;
     background-color: #ddd;
     overflow: hidden;
-  }
-
-  .upload_button:after {
-    position: absolute;
-    top: 30px;
-    left: 0;
-    z-index: 999;
-    transition: top 0.1s;
-    box-sizing: border-box;
-    padding-left: 25px;
-    width: 100%;
-    height: 100%;
-    content: 'loading...';
-    text-align: left;
-    background: url('./loading.gif') no-repeat 5px center #eee;
-    background-size: 15px 15px;
-    color: #999;
   }
 
   .upload_button.loading {
@@ -396,14 +167,6 @@ export default defineComponent({
     position: absolute;
     left: 50%;
     transform: translate(-50%, -50%);
-  }
-
-  .upload_drag .icon {
-    top: 40%;
-    width: 80px;
-    height: 62px;
-    background: url('upload.png') no-repeat;
-    background-size: 100% 100%;
   }
 
   .upload_drag .text {
