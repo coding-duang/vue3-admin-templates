40ecb8c9a0733d1dfafe4d59f8852f0db2a8fa6d
Dai <62934849+dai-oh@users.noreply.github.com>
2023-02-21 10:15:53 +0800

    feat(framework-主项目): 🎸 去除无用的console.log

--- src/pages/Components/Editor/callback.ts
+++ src/pages/Components/Editor/callback.ts
@@ -4,27 +4,15 @@ import { Editor } from '@wangeditor/editor-for-vue'
 import { IDomEditor } from '@wangeditor/editor'
 
 export const useCallbacks = () => {
-  const handleChange = (editor: EditorType) => {
-    console.log('change:', editor.getHtml())
-  }
-  const handleDestroyed = (editor: EditorType) => {
-    console.log('destroyed', editor)
-  }
-  const handleFocus = (editor: EditorType) => {
-    console.log('focus', editor)
-  }
-  const handleBlur = (editor: EditorType) => {
-    console.log('blur', editor)
-  }
-  const customAlert = (info: string, type: string) => {
-    alert(`【自定义提示】${type} - ${info}`)
-  }
+  const handleChange = (editor: EditorType) => {}
+  const handleDestroyed = (editor: EditorType) => {}
+  const handleFocus = (editor: EditorType) => {}
+  const handleBlur = (editor: EditorType) => {}
+  const customAlert = (info: string, type: string) => {}
   const customPaste = (editor: EditorType, event: Event) => {
     console.log('ClipboardEvent 粘贴事件对象', event)
-
     // 自定义插入内容
     editor.insertText('xxx')
-
     // 返回值（注意，vue 事件的返回值，不能用 return）
     // callback(true) // 返回 true ，继续默认的粘贴行为
   }
