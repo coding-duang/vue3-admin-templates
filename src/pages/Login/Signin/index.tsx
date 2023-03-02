c3e0e881ebcb7b0acfd5ee50801b86ae22f7d21d
Dai <62934849+dai-oh@users.noreply.github.com>
2023-03-02 22:32:06 +0800

    feat(framework-主项目): 🎸 完成表单验证

--- src/pages/Login/Signin/index.tsx
+++ src/pages/Login/Signin/index.tsx
@@ -1,9 +1,11 @@
-import { onMounted, reactive, ref } from 'vue'
-import { FormEnum, FormValuesType } from '@/types'
-import { FormInst, NButton, NForm, NFormItem, NIcon, NInput } from 'naive-ui'
-
 import { Glasses, GlassesOutline, SwapHorizontal } from '@vicons/ionicons5'
+import { FormInst, NButton, NForm, NFormItem, NIcon, NInput } from 'naive-ui'
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { getSignInRules, useValidate } from '../Common/Validate'
+
+import { FormEnum, FormValuesType } from '@/types'
+
 import styles from './index.module.scss'
 
 export default defineComponent({
@@ -11,74 +13,18 @@ export default defineComponent({
   setup() {
     const router = useRouter()
 
-    const values = reactive<FormValuesType>({
+    const formData = ref<FormValuesType>({
       [FormEnum.PASSWORD]: '',
       [FormEnum.MOBILE]: '',
     })
 
     const fromRef = ref<FormInst | null>(null)
 
-    const phoneCheck = () => {
-      const regex =
-        /^1(3\d|4[01456879]|5[0-3,5-9]|6[2567]|7[0-8]|8[0-9]|9[0-3,5-9])\d{8}$/
-      if (!regex.test(values.mobile)) {
-        return false
-      }
-      return true
-    }
-    const passwordCheck = () => {
-      // 8-20位数字和字母组合
-      const regex = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,20}$/
+    const { validatePhone, validatePassword } = useValidate(formData)
 
-      if (!regex.test(values.password)) {
-        return false
-      }
-      return true
-    }
-
-    // validation
-    const phoneValidationStatus = computed(() => {
-      if (!values.mobile) {
-        return 'error'
-      }
-
-      if (!phoneCheck()) {
-        return 'error'
-      }
-
-      return undefined
-    })
-
-    const phoneFeedback = computed(() => {
-      if (!values.mobile) {
-        return '请输入手机号码'
-      }
-
-      if (!phoneCheck()) {
-        return '请输入正确的手机号码'
-      }
-
-      return ''
-    })
-
-    const passwordValidationStatus = computed(() => {
-      if (!values.password) {
-        return 'error'
-      }
-
-      if (!passwordCheck()) {
-        return 'error'
-      }
-      return undefined
-    })
-
-    const passwordFeedback = computed(() => {
-      if (!values.password) {
-        return '请输入密码'
-      }
-      if (!passwordCheck()) {
-        return '请输入正确的密码'
-      }
+    const rules = getSignInRules({
+      validatePhone,
+      validatePassword,
     })
 
     onMounted(() => {
@@ -98,31 +44,20 @@ export default defineComponent({
             <SwapHorizontal />
           </NIcon>
         </p>
-        <NForm ref={fromRef}>
-          <NFormItem
-            label="账号"
-            label-placement="top"
-            class={styles.item}
-            validation-status={phoneValidationStatus.value}
-            feedback={phoneFeedback.value}
-          >
+        <NForm ref={fromRef} model={formData} rules={rules}>
+          <NFormItem label="账号" label-placement="top" path="mobile">
             <NInput
-              v-model={[values.mobile, 'model-value']}
+              v-model={[formData.value.mobile, 'value']}
               clearable
               placeholder="请输入11位手机号码"
             />
           </NFormItem>
 
-          <NFormItem
-            label="密码"
-            label-placement="top"
-            validation-status={passwordValidationStatus.value}
-            feedback={passwordFeedback.value}
-          >
+          <NFormItem label="密码" label-placement="top" path="password">
             <NInput
               type="password"
               show-password-on="click"
-              v-model={[values.password, 'model-value']}
+              v-model={[formData.value.password, 'value']}
             >
               {{
                 passwordVisibleIcon: () => (
--- src/pages/Login/Signup/index.tsx
+++ src/pages/Login/Signup/index.tsx
@@ -1,17 +1,8 @@
-import { FormValuesType, FormEnum } from '@/types'
+import { FormEnum, FormValuesType } from '@/types'
 import { Glasses, GlassesOutline, SwapHorizontal } from '@vicons/ionicons5'
-import { values } from 'lodash'
-import {
-  NIcon,
-  NForm,
-  NFormItem,
-  NInput,
-  FormInst,
-  FormItemRule,
-  NButton,
-  FormRules,
-} from 'naive-ui'
+import { FormInst, NButton, NForm, NFormItem, NIcon, NInput } from 'naive-ui'
 import { useRouter } from 'vue-router'
+import { getSignUpRules, useValidate } from '../Common/Validate'
 import styles from './index.module.scss'
 
 export default defineComponent({
@@ -26,62 +17,14 @@ export default defineComponent({
       [FormEnum.REPEAT_PASSWORD]: '',
     })
 
-    function validatePassword(): boolean {
-      const regex = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,20}$/
+    const { validatePhone, validatePassword, validatePasswordSame } =
+      useValidate(formData)
 
-      console.log(regex.test(formData.value.password), formData.value.password)
-
-      if (!regex.test(formData.value.password)) {
-        return false
-      }
-      return true
-    }
-
-    function validatePasswordSame(): boolean {
-      console.log(formData.value.password, formData.value.reenteredPassword)
-      return formData.value.password === formData.value.reenteredPassword
-    }
-
-    function validatePhone(): boolean {
-      const regex =
-        /^1(3\d|4[01456879]|5[0-3,5-9]|6[2567]|7[0-8]|8[0-9]|9[0-3,5-9])\d{8}$/
-
-      if (!regex.test(formData.value.mobile)) {
-        return false
-      }
-
-      return true
-    }
-
-    const rules: FormRules = {
-      mobile: [
-        {
-          required: true,
-          message: '请输入手机号码',
-        },
-        {
-          validator: validatePhone,
-          message: '请输入正确的手机号码',
-          trigger: ['input', 'blur'],
-        },
-      ],
-      password: [
-        {
-          required: true,
-          validator: validatePassword,
-          message: '请输入8-20位数字和字母组合',
-          trigger: ['blur', 'input'],
-        },
-      ],
-      reenteredPassword: [
-        {
-          required: true,
-          validator: validatePasswordSame,
-          message: '两次密码输入不一致',
-          trigger: ['blur', 'input'],
-        },
-      ],
-    }
+    const rules = getSignUpRules({
+      validatePhone,
+      validatePassword,
+      validatePasswordSame,
+    })
 
     return () => (
       <div class={styles.loginWrap}>
